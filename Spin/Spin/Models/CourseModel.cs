b6ea07b5bda6708600f67d059cd38c3b273990aa
scottharben@gmail.com <scottharben@gmail.com>
2019-02-07 08:53:27 -0500

    Code First Migrations CourseModel added

---
+++ Spin/Spin/Models/CourseModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spin.Models
+{
+    public class CourseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Par { get; set; }
+        public int Hole1_Par { get; set; }
+        public int Hole2_Par { get; set; }
+        public int Hole3_Par { get; set; }
+        public int Hole4_Par { get; set; }
+        public int Hole5_Par { get; set; }
+        public int Hole6_Par { get; set; }
+        public int Hole7_Par { get; set; }
+        public int Hole8_Par { get; set; }
+        public int Hole9_Par { get; set; }
+        public int Hole10_Par { get; set; }
+        public int Hole11_Par { get; set; }
+        public int Hole12_Par { get; set; }
+        public int Hole13_Par { get; set; }
+        public int Hole14_Par { get; set; }
+        public int Hole15_Par { get; set; }
+        public int Hole16_Par { get; set; }
+        public int Hole17_Par { get; set; }
+        public int Hole18_Par { get; set; }
+    }
+}
--- Spin/Spin/Models/IdentityModels.cs
+++ Spin/Spin/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Spin.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<CourseModel> Course { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
