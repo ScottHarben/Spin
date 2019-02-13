758b7713100552eb59560ce114a7978caac021e3
scottharben@gmail.com <scottharben@gmail.com>
2019-02-12 19:18:17 -0500

    Kept <a> links, created passable round/view Models

--- Spin/Spin/Controllers/NewRoundController.cs
+++ Spin/Spin/Controllers/NewRoundController.cs
@@ -39,13 +39,38 @@ namespace Spin.Controllers
         
         public ActionResult ChooseTees(int courseId)
         {
-            var tees = _context.TeeModels.Where(c => c.CourseModelId == courseId);
+            var tees = _context.TeeModels.Where(c => c.CourseModelId == courseId).ToList();
             var viewModel = new NewRoundViewModel
             {
                 TeeModels = tees
             };
             
             return View(viewModel);
+        }
+
+        public ActionResult SetupRound(int teeId)
+        {
+            var holes = _context.HoleModels.Where(c => c.TeeModelId == teeId).ToList();
+            var tees = _context.TeeModels.FirstOrDefault(c => c.Id == teeId);
+            var tee = tees.Id;
+            var course = tees.CourseModelId;
+            var roundModel = new RoundModel
+            {
+                CourseModelId = course,
+                TeeModelId = tee,
+            };
+            var viewModel = new NewRoundViewModel
+            {
+                HoleModels = holes,
+                RoundModel = roundModel,
+            };
+
+            return RedirectToAction("PlayingRound", "NewRound");
+        }
+
+        public ActionResult PlayingRound(RoundModel roundModel, NewRoundViewModel viewModel)
+        {
+            return View();
         }
     }
 }
